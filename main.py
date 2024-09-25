5292d1bce21e1b379910738a22488c064fba021c
Meryem Yahya <47725652+MeryemYahya@users.noreply.github.com>
2024-09-25 15:46:55 +0100

    fix issue in main.py

--- main.py
+++ main.py
@@ -3,17 +3,25 @@ import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
-from sklearn import datasets
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score, recall_score
-from sklearn.metrics import plot_confusion_matrix
+#from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+
+
+st.title('Binary Classification WebApp')
+st.write("Results")
+
+# Same treatment as in Jupeter nootbook
+
 # Import data
 df_test = pd.read_csv('test.csv')
 df_train = pd.read_csv('train.csv')
@@ -28,33 +36,41 @@ df_train['Cabin'].fillna(df_train['Cabin'].mode()[0], inplace=True)
 df_train['Embarked'].fillna(df_train['Embarked'].mode()[0], inplace=True)
 
 # Preprocessing data
-df_train = df_train.drop(['PassengerId'], 1)
-df_train['Pclass'].unique()
+df_train = df_train.drop(['PassengerId'], axis=1)
+
 df_train['Title'] = df_train['Name'].str.extract('([A-Za-z]+)\.',)
-df_train['Title'].unique()
-df_train = df_train.drop(['Name'], 1)
+
+df_train = df_train.drop(['Name'], axis=1)
 df_train['Fare_Band'] = pd.cut(df_train['Fare'], 3)
-df_train['Fare_Band'].unique()
+
 df_train.loc[(df_train['Fare'] <= 170.776), 'Fare'] = 1
 df_train.loc[(df_train['Fare'] > 170.776) & (
     df_train['Fare'] <= 314.553), 'Fare'] = 2
 df_train.loc[(df_train['Fare'] > 314.553) & (
     df_train['Fare'] <= 513), 'Fare'] = 3
-df_train = df_train.drop(['Fare_Band'], 1)
+df_train = df_train.drop(['Fare_Band'], axis=1)
+
+
 df_train['FamilySize'] = df_train['SibSp']+df_train['Parch']+1
-df_train = df_train.drop(['SibSp', 'Parch'], 1)
-df_train = df_train.drop(['Ticket'], 1)
-df_train['Cabin'].unique()
+
+df_train = df_train.drop(['SibSp', 'Parch'], axis=1)
+
+df_train = df_train.drop(['Ticket'], axis=1)
+
+
 df_train['Cabin'] = df_train['Cabin'].fillna('U')
 df_train['Cabin'] = df_train['Cabin'].astype(str).str[0]
-df_train.Cabin.unique()
-df_train = df_train.drop(['Cabin'], 1)
+
+df_train = df_train.drop(['Cabin'], axis=1)
+
 # get dummies
 df_train = pd.get_dummies(
     columns=['Pclass', 'Sex', 'Embarked', 'Title', 'Age', 'Fare'], data=df_train)
 X = df_train.drop('Survived', axis=1)
 y = df_train['Survived']
-# Transform
+
+
+# Transform and split
 scaler = StandardScaler()
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.1, random_state=4)
@@ -64,34 +80,13 @@ scaler.fit(X_test)
 X_test = scaler.transform(X_test)
 
 
-st.title('Binary Classification WebApp')
-
-st.write("Results")
-
-#dataset_name = st.sidebar.selectbox( 'Select Dataset',('Iris', 'Breast Cancer', 'Wine'))
-
-# st.write(f"## {dataset_name} Dataset")
-
+# Classifier SelectBox
 classifier_name = st.sidebar.selectbox(
     'Select classifier',
     ('KNN', 'Logistic Regression', 'Random Forest')
 )
 
-# def get_dataset(name):
-#data = None
-# if name == 'Iris':
-#     data = datasets.load_iris()
-# elif name == 'Wine':
-#     data = datasets.load_wine()
-# else:
-#      data = datasets.load_breast_cancer()
-#   X = data.data
-#   y = data.target
-#  return X, y
-
-#X, y = get_dataset(dataset_name)
-#st.write('Shape of dataset:', X.shape)
-#st.write('number of classes:', len(np.unique(y)))
+# Model Params
 
 
 def add_parameter_ui(clf_name):
@@ -112,6 +107,8 @@ def add_parameter_ui(clf_name):
 
 params = add_parameter_ui(classifier_name)
 
+# Selected Model and params
+
 
 def get_classifier(clf_name, params):
     clf = None
@@ -126,42 +123,25 @@ def get_classifier(clf_name, params):
 
 
 clf = get_classifier(classifier_name, params)
+
 #### CLASSIFICATION ####
 
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 
 
 st.write(f'Classifier = {classifier_name}')
-st.write(f'Accuracy =', accuracy_score(y_test, y_pred).round(2))
 
+# Results
+st.write(f'Accuracy =', accuracy_score(y_test, y_pred).round(2))
 st.write("Precision: ", precision_score(y_test, y_pred).round(2))
 st.write("Recall: ", recall_score(y_test, y_pred).round(2))
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+#st.set_option('deprecation.showPyplotGlobalUse', False)
 
-
+# Confusion Matrix
 st.subheader("Confusion Matrix")
-plot_confusion_matrix(clf, X_test, y_test)
+#plot_confusion_matrix(clf, X_test, y_test)
+ConfusionMatrixDisplay.from_estimator(clf,X_test,y_test)
 st.pyplot()
-
-# plot_metrics(metrics)
-#### PLOT DATASET ####
-# Project the data onto the 2 primary principal components
-#pca = PCA(2)
-#X_projected = pca.fit_transform(X)
-
-#x1 = X_projected[:, 0]
-#x2 = X_projected[:, 1]
-
-#fig = plt.figure()
-#plt.scatter(x1, x2,    c=y, alpha=0.8,    cmap='viridis')
-
-#plt.xlabel('Principal Component 1')
-#plt.ylabel('Principal Component 2')
-# plt.colorbar()
-
-# plt.show()
-# t.pyplot(fig)
